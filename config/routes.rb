d00512ace1cbb1e37726b504bdd0026368ba6e76
William Ross <will@spanner.org>
2013-12-28 16:50:18 +0000

    priority to custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   get '/users/sign_in' => "user_sessions#new", as: "sign_in"
   post '/users/sign_in' => "user_sessions#create"
   delete '/users/sign_out' => "user_sessions#destroy", as: "sign_out"
+  get '/users/preferences' => "users#edit", as: "preferences"
+  get "/users/:id/welcome/:tok" => "users#welcome", as: "welcome"
 
   resources :users do
     put :confirm, on: :member
   end
-  get "/users/:id/welcome/:tok" => "users#welcome", as: "welcome"
-  get '/users/preferences' => "users#edit", as: "preferences"
   
 end
