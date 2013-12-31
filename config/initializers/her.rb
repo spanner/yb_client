c5ef049d0be4686a72e37d4c64fc59a02bc6c80d
William Ross <will@spanner.org>
2013-12-31 21:20:04 +0000

    new her api interface

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,7 +1,8 @@
 require 'settings'
 require 'paginated_her'
 
-CDB = Her::API.new base_uri: "#{Settings.cdb.protocol}://#{Settings.cdb.host}/api" do |c|
+CDB = Her::API.new
+CDB.setup url: "#{Settings.cdb.protocol}://#{Settings.cdb.host}/api" do |c|
   c.use Faraday::Request::UrlEncoded
   c.use PaginatedHer::Middleware::TokenAuth
   c.use PaginatedHer::Middleware::Parser
