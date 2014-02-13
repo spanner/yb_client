5f603d77e7d9db49a80701ccdf14ec7fa938155e
William Ross <will@spanner.org>
2014-02-13 09:51:29 +0000

    decache for notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,6 +4,8 @@ class Note
   belongs_to :person, foreign_key: :person_uid
   collection_path "/api/people/:person_uid/notes"
   
+  after_save :decache
+  
   def self.new_with_defaults(attributes={})
     self.new({
       title: "", 
@@ -13,6 +15,17 @@ class Note
   
   def date
     DateTime.parse(created_at)
+  end
+
+  protected
+  
+  def decache(and_associates=true)
+    if $cache
+      path = self.class.collection_path
+      $cache.delete path
+      $cache.delete "#{path}/#{self.to_param}"
+      self.person.send(:decache, false) if and_associates && self.person
+    end
   end
 
 end
